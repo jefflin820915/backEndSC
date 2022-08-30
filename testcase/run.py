c3e0b42c2e89888d653b73cc3c57c5f1321d7c6b
jefflin820 <tzuen.lin@ennowell.com>
2022-08-30 13:59:21 +0800

    830

--- testcase/run.py
+++ testcase/run.py
@@ -1,4 +1,6 @@
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['-s', '-v', '--capture=sys', 'accountManage/test_userLog.py'])
+    pytest.main(['-s', '-v', '--capture=sys'])
+    # 跑單項測試案例
+    # pytest.main(['-s', '-v', '--capture=sys', 'accountManage/test_userLog.py'])
